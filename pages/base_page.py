7a985476521449f803214d25170c212e906ecaab
Смирнов Василий Васильевич <vsmirnov@msk.termika.ru>
2021-09-13 13:53:01 +0300

    Добавление базовой страницы BasePage (Page Objec)

---
+++ pages/base_page.py
@@ -0,0 +1,11 @@
+﻿class BasePage():
+	
+	# конструктор стандартной страницы
+	def __init__(self, browser, url):
+		self.browser = browser
+		self.url = url
+	
+	# открытие страницы
+	def open(self):
+		self.browser.get(self.url)
+#
