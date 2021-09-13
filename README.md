ab8790a113921a6549a65974994ca8e902b1a5bf
Смирнов Василий Васильевич <vsmirnov@msk.termika.ru>
2021-09-13 13:29:59 +0300

    Подготовка проекта к запуску

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # selenium-stepik-project
 Тестовый проект для финального задания
+
+pytest -v --tb=line --language=en test_main_page.py
---
+++ conftest.py
@@ -0,0 +1,29 @@
+﻿import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+	parser.addoption('--browser_name', action='store', default="chrome",
+		help="Choose browser: chrome or firefox")
+	parser.addoption('--language', action='store', default="en", 
+		help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+	browser_name = request.config.getoption("browser_name")
+	browser = None
+	if browser_name == "chrome":
+		print("\nstart chrome browser for test..")
+		#browser = webdriver.Chrome()
+		browser = webdriver.Chrome(r"D:\chromedriver\chromedriver.exe")
+	elif browser_name == "firefox":
+		print("\nstart firefox browser for test..")
+		#browser = webdriver.Firefox()
+		browser = webdriver.Firefox(executable_path = r"D:\geckodriver\geckodriver.exe")
+	else:
+		raise pytest.UsageError("--browser_name should be chrome or firefox")
+	yield browser
+	# этот код выполнится после завершения теста
+	print("\nquit browser..")
+	browser.quit()
+#
