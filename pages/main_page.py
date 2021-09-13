edd166c6b93324ec39faa6b4c60cbb58acfd7626
Смирнов Василий Васильевич <vsmirnov@msk.termika.ru>
2021-09-13 15:20:26 +0300

    Добавление базовой страницы BasePage (Page Objec)

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,1 +1,14 @@
-﻿
+﻿from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage):
+	
+	#переход на страницу авторизации
+	def go_to_login_page(self):
+		login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+		login_link.click() 
+		
+	#проверка ссылки на авторизацию
+	def should_be_login_link(self):
+		assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+#
