ee911881024ca9f01fc38931daad92dfa3325a7b
Смирнов Василий Васильевич <vsmirnov@msk.termika.ru>
2021-09-16 07:40:38 +0300

    изменил метку review2 на review

--- test_product_page.py
+++ test_product_page.py
@@ -121,7 +121,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 	page.go_to_login_page()
 
 #@pytest.mark.skip
-@pytest.mark.need_review2
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	# Гость открывает страницу товара
 	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
